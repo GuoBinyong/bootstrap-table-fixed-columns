8116963683291b3c866564940a8e7f5c72901153
郭斌勇 <guobinyong@qq.com>
2018-11-17 16:12:30 +0800

    更改核心代码和调用机制，解决原项目中的部分bug

--- README.md
+++ README.md
@@ -4,19 +4,33 @@
 本项目 从 [源项目][] Fork 而来，旨在解决原项目的bug； 目前已经解决了如下 bug:
 
 # 已解决的问题
-- 通过 bootstrapTable 的 mergeCells 方法合并单元格时，冻结的列标题没有合并效果；
-- 修复了单元格跨行合并时，冻结的列标题的所有列都会进行相应的行合并的bug；
-- 优化了部分代码；
-- 修复了改变窗口尺寸时，冻结的列不会自动调整大小的bug;
+ - 更改核心代码的逻辑，使冻结列的内容和样式与 bootstrapTable 尽量一致，以免其它潜在的bug；
+ - 通过 bootstrapTable 的 mergeCells 方法合并单元格时，冻结的列标题没有合并效果；
+ - 修复了单元格跨行合并时，冻结的列标题的所有列都会进行相应的行合并的bug；
+ - 优化了部分代码；
+ - 修复了改变窗口尺寸时，冻结的列不会自动调整大小的bug;
+ - 修复了冻结列的底部露出非标题内容的bug；
 
 
 # 新增方法
 我给 bootstrapTable 增加了如下方法：
 
-**冻结列：**  
+- 初始化冻结列组件：
+
 ```
-$('#table').bootstrapTable('fixedColumns', fixedNumber);
+$('#table').bootstrapTable('initFixedColumns', fixedNumber);
 @param fixedNumber : number 冻结的列数
+```
+
+
+# 注意
+- 如果冻结的列中不能有空单元格，如果需要空单元格，可在单元格中插入空格字符
+
+
+- 刷新冻结列组件
+
+```
+$('#table').bootstrapTable('initFixedColumns');
 ```
 
 
--- bootstrap-table-fixed-columns.js
+++ bootstrap-table-fixed-columns.js
@@ -3,19 +3,34 @@
  * @version: v1.0.2
  *
  * 修复了源项目的如下bug：
+ * - 更改核心代码的逻辑，使冻结列的内容和样式与 bootstrapTable 尽量一致，以免其它潜在的bug；
  * - 通过 bootstrapTable 的 mergeCells 方法合并单元格时，冻结的列标题没有合并效果；
  * - 修复了单元格跨行合并时，冻结的列标题的所有列都会进行相应的行合并的bug；
  * - 优化了部分代码；
  * - 修复了改变窗口尺寸时，冻结的列不会自动调整大小的bug;
+ * - 修复了冻结列的底部露出非标题内容的bug；
+ *
  *
  * # 新增方法
  * 我给 bootstrapTable 增加了如下方法：
  *
- * - 冻结列：
+ * - 初始化冻结列组件：
  * ```
- * $('#table').bootstrapTable('fixedColumns', fixedNumber);
+ * $('#table').bootstrapTable('initFixedColumns', fixedNumber);
  * @param fixedNumber : number 冻结的列数
  * ```
+ *
+ *
+ * - 刷新冻结列组件
+ * ```
+ * $('#table').bootstrapTable('initFixedColumns');
+ * ```
+ * 
+ * 
+ * 
+ * # 注意
+ * - 如果冻结的列中不能有空单元格，如果需要空单元格，可在单元格中插入空格字符
+ * 
  */
 
 (function ($) {
@@ -26,184 +41,108 @@
         fixedNumber: 1
     });
 
-    var BootstrapTable = $.fn.bootstrapTable.Constructor,
-        _initHeader = BootstrapTable.prototype.initHeader,
-        _initBody = BootstrapTable.prototype.initBody,
-        _resetView = BootstrapTable.prototype.resetView;
-
-
-
-    //重载mergeCells：开始
-
-    //保存原来的 mergeCells 方法
-    var _mergeCells = BootstrapTable.prototype.mergeCells;
-
-    //重载 mergeCells 方法
-    BootstrapTable.prototype.mergeCells = function(){
-        _mergeCells.apply(this, Array.prototype.slice.apply(arguments));
-
-        //在 mergeCells 方法被调用后，初始化对冻结列进行初始化
-        this.fixedColumns();
-    }
-    //重载mergeCells：结束
+    var BootstrapTable = $.fn.bootstrapTable.Constructor;
 
 
 
 
 
-    //重载resetWidth：开始
-    var _resetWidth = BootstrapTable.prototype.resetWidth;
 
-    BootstrapTable.prototype.resetWidth = function(){
 
-        _resetWidth.apply(this, Array.prototype.slice.apply(arguments));
+    //重载 initContainer：开始
+    var _initContainer = BootstrapTable.prototype.initContainer ;
+
+    BootstrapTable.prototype.initContainer = function(){
+        _initContainer.apply(this, Array.prototype.slice.apply(arguments));
 
         if (!this.options.fixedColumns) {
             return;
         }
 
-        this.resetViewForFixedColumns();
+        this.initContainerForFixedColumns();
     }
 
-    //重载resetWidth：结束
+
+    BootstrapTable.prototype.initContainerForFixedColumns = function () {
+          //$fixedHeader：开始
+          this.$fixedHeader = $('<div class="fixed-table-header-columns">');
+          this.timeoutHeaderColumns_ = 0;
+          this.$tableHeader.before(this.$fixedHeader);
+        //$fixedHeader：结束
 
 
 
-
-
-    /**
-     * 冻结列
-     * @param fixedNumber : number 冻结的列数
-     */
-    BootstrapTable.prototype.fixedColumns = function(fixedNumber){
-
-        if  (fixedNumber != null){
-            this.options.fixedColumns = true ;
-            this.options.fixedNumber = fixedNumber ;
-        }
-
-        if (!this.options.fixedColumns) {
-            return;
-        }
-
-        //根据源项目的实际调用顺序来设置以下3个方法的调用顺序
-        this.initHeaderForFixedColumns();
-        this.resetViewForFixedColumns();
-        this.initBodyForFixedColumns();
-
-    };
-
-
-    /*
-
-        BootstrapTable.prototype.initFixedColumns = function () {
-            this.$fixedHeader = $([
-                '<div class="fixed-table-header-columns">',
-                '<table>',
-                '<thead></thead>',
-                '</table>',
-                '</div>'].join(''));
-
-            this.timeoutHeaderColumns_ = 0;
-            this.$fixedHeader.find('table').attr('class', this.$el.attr('class'));
-            this.$fixedHeaderColumns = this.$fixedHeader.find('thead');
-            this.$tableHeader.before(this.$fixedHeader);
-
-            this.$fixedBody = $([
-                '<div class="fixed-table-body-columns">',
-                '<table>',
-                '<tbody></tbody>',
-                '</table>',
-                '</div>'].join(''));
-
-            this.timeoutBodyColumns_ = 0;
-            this.$fixedBody.find('table').attr('class', this.$el.attr('class'));
-            this.$fixedBodyColumns = this.$fixedBody.find('tbody');
-            this.$tableBody.before(this.$fixedBody);
-        };
-    */
-
-    BootstrapTable.prototype.initFixedColumns = function () {
-        this.$fixedHeader = $('<div class="fixed-table-header-columns">');
-
-        var tableHeader = this.$tableHeader.children("table");
-        var tableHeaderClone = tableHeader.clone(true);
-        tableHeaderClone.addClass(this.$el.attr('class'));
-
-        var headerClone = this.$header.clone(true);
-        tableHeaderClone.append(headerClone);
-        this.$fixedHeader.append(tableHeaderClone);
-
-
-        this.timeoutHeaderColumns_ = 0;
-
-        this.$fixedHeaderColumns = headerClone;
-        this.$tableHeader.before(this.$fixedHeader);
-
-
-
+        //$fixedBody：开始
         this.$fixedBody = $('<div class="fixed-table-body-columns">');
 
-        var fixedBodyTable = $('<table>');
-        fixedBodyTable.addClass(this.$el.attr('class'));
+        var $fixedBodyTable = $('<table>');
+        $fixedBodyTable.addClass(this.$el.attr('class'));
 
-        this.$fixedBodyColumns = $('<tbody>');
+        this.$fixedBody.append($fixedBodyTable);
+        this.$fixedBodyTable = $fixedBodyTable ;
 
-        fixedBodyTable.append(this.$fixedBodyColumns);
-        this.$fixedBody.append(fixedBodyTable);
+          this.timeoutBodyColumns_ = 0;
+          this.$tableBody.before(this.$fixedBody);
+        //$fixedBody：结束
 
+      };
 
-
-        this.timeoutBodyColumns_ = 0;
-        this.$tableBody.before(this.$fixedBody);
-    };
-
-
+    //重载 initContainer：结束
 
 
-    BootstrapTable.prototype.initHeaderForFixedColumns = function () {
-
-        this.initFixedColumns();
-
-        var that = this, $trs = this.$header.find('tr').clone();
-        $trs.each(function () {
-            $(this).find('th:gt(' + that.options.fixedNumber + ')').remove();
-        });
-        this.$fixedHeaderColumns.html('').append($trs);
-    };
 
 
-    BootstrapTable.prototype.initHeader = function () {
-        _initHeader.apply(this, Array.prototype.slice.apply(arguments));
+
+    // 重载 fitHeader：开始
+
+    var _fitHeader = BootstrapTable.prototype.fitHeader ;
+    BootstrapTable.prototype.fitHeader = function () {
+        _fitHeader.apply(this, Array.prototype.slice.apply(arguments));
 
         if (!this.options.fixedColumns) {
             return;
         }
 
-        this.initHeaderForFixedColumns();
+        this.resetHeaderForFixedColumns();
     };
 
 
+    BootstrapTable.prototype.resetHeaderForFixedColumns = function () {
 
+        var tableHeader = this.$tableHeader.children("table");
+        var tableHeaderClone = tableHeader.clone(true,true);
+        tableHeaderClone.removeAttr("style");
 
+        var fixedNumber = this.options.fixedNumber
 
-    BootstrapTable.prototype.initBodyForFixedColumns = function () {
+        var $trs = tableHeaderClone.find('tr');
 
-        var that = this;
+        $trs.each(function (trIndex,tr) {
 
-        this.$fixedBodyColumns.html('');
-        this.$body.find('> tr[data-index]').each(function () {
-            var $tr = $(this).clone(),
-                $tds = $tr.find('td');
+            var totalColspan = 0;
 
-            $tr.html('');
-            var end = that.options.fixedNumber;
-            for (var i = 0; i < end; i++) {
-                $tr.append($tds.eq(i).clone());
-            }
-            that.$fixedBodyColumns.append($tr);
+            $(tr).children().filter(function (thIndex) {
+                 var colspan = $(this).attr('colspan') || 1 ;
+                 totalColspan += colspan;
+
+                 return totalColspan > fixedNumber ;
+            }).remove();
         });
+
+        this.$fixedHeader.empty();
+        this.$fixedHeader.append(tableHeaderClone);
     };
+
+    // 重载 fitHeader：结束
+
+
+
+
+
+
+    // 重载 initBody：开始
+
+    var _initBody = BootstrapTable.prototype.initBody;
 
     BootstrapTable.prototype.initBody = function () {
         _initBody.apply(this, Array.prototype.slice.apply(arguments));
@@ -215,6 +154,49 @@
     };
 
 
+    BootstrapTable.prototype.initBodyForFixedColumns = function () {
+
+        var $fixedBodyTbody = this.$body.clone(true,true);
+
+        var fixedNumber = this.options.fixedNumber;
+
+
+        var $trs = $fixedBodyTbody.children();
+
+        $trs.each(function (trIndex,tr) {
+
+            var totalColspan = 0;
+
+            $(tr).children().filter(function (thIndex) {
+                var colspan = $(this).attr('colspan') || 1 ;
+                totalColspan += colspan;
+
+                return totalColspan > fixedNumber ;
+            }).remove();
+        });
+
+        this.$fixedBodyTable.empty();
+        this.$fixedBodyTable.append($fixedBodyTbody);
+
+    };
+
+    // 重载 initBody：结束
+
+
+
+    // 重载 resetView：开始
+
+    var _resetView = BootstrapTable.prototype.resetView;
+
+    BootstrapTable.prototype.resetView = function () {
+        _resetView.apply(this, Array.prototype.slice.apply(arguments));
+
+        if (!this.options.fixedColumns) {
+            return;
+        }
+        this.resetViewForFixedColumns();
+    };
+
 
     BootstrapTable.prototype.resetViewForFixedColumns = function () {
 
@@ -225,14 +207,9 @@
         this.timeoutBodyColumns_ = setTimeout($.proxy(this.fitBodyColumns, this), this.$el.is(':hidden') ? 100 : 0);
     };
 
-    BootstrapTable.prototype.resetView = function () {
-        _resetView.apply(this, Array.prototype.slice.apply(arguments));
+    // 重载 resetView：开始
 
-        if (!this.options.fixedColumns) {
-            return;
-        }
-        this.resetViewForFixedColumns();
-    };
+
 
     BootstrapTable.prototype.fitHeaderColumns = function () {
         var that = this,
@@ -258,10 +235,13 @@
         this.$fixedHeader.width(headerWidth + 1).show();
     };
 
+
+
     BootstrapTable.prototype.fitBodyColumns = function () {
         var that = this,
             top = -(parseInt(this.$el.css('margin-top')) - 2),
             // the fixed height should reduce the scorll-x height
+            //后面的数字可用于调节tbody的冻结列 $fixedBody 的高度与$tableBody的差值
             height = this.$tableBody.height() - 14;
 
         if (!this.$body.find('> tr[data-index]').length) {
@@ -306,12 +286,118 @@
 
 
 
-    //
+
+
+
+
+
+
+
+
+    //重载 mergeCells：开始
+
+    //保存原来的 mergeCells 方法
+    var _mergeCells = BootstrapTable.prototype.mergeCells;
+
+    //重载 mergeCells 方法
+    BootstrapTable.prototype.mergeCells = function(){
+        _mergeCells.apply(this, Array.prototype.slice.apply(arguments));
+
+        //在 mergeCells 方法被调用后，刷新冻结列组件
+        this.refreshFixedColumns();
+    }
+    //重载 mergeCells：结束
+
+
+
+
+
+    //重载 resetWidth：开始
+    var _resetWidth = BootstrapTable.prototype.resetWidth;
+
+    BootstrapTable.prototype.resetWidth = function(){
+
+        _resetWidth.apply(this, Array.prototype.slice.apply(arguments));
+
+        if (!this.options.fixedColumns) {
+            return;
+        }
+
+        this.resetViewForFixedColumns();
+    }
+
+    //重载resetWidth：结束
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //初始化冻结列：开始
 
     /**
-     * 往 bootstrapTable 中注入新的方法 fixedColumns
-     * 参数是 fixedNumber ，数字类型，表示冻结的列数
+     * 初始化冻结列组件
+     * @param fixedNumber : number 冻结的列数
      */
-    $.fn.bootstrapTable.methods.push("fixedColumns")
+    BootstrapTable.prototype.initFixedColumns = function(fixedNumber){
+
+        if  (fixedNumber != null){
+            this.options.fixedColumns = true ;
+            this.options.fixedNumber = fixedNumber ;
+        }
+
+        if (!this.options.fixedColumns) {
+            return;
+        }
+
+
+        this.initContainerForFixedColumns();    //初始化容器
+        this.refreshFixedColumns();     //刷新冻结列组件
+
+    };
+
+
+
+    /**
+     * 刷新冻结列组件
+     */
+    BootstrapTable.prototype.refreshFixedColumns = function(){
+
+        if (!this.options.fixedColumns) {
+            return;
+        }
+
+        this.resetHeaderForFixedColumns();      //重设冻结列的标题行
+        this.initBodyForFixedColumns();         //重置冻结列的标题列
+        this.resetViewForFixedColumns();        //重置view
+
+    };
+
+
+
+    /**
+     * 往 bootstrapTable 中注入以下新的方法
+     * - initFixedColumns ： 初始化冻结列组件
+     * 参数是 fixedNumber ，数字类型，表示冻结的列数
+     *
+     * - initFixedColumns : 刷新冻结列组件
+     */
+    $.fn.bootstrapTable.methods.push("initFixedColumns","refreshFixedColumns");
+
+    //初始化冻结列：结束
+
+
 
 })(jQuery);
